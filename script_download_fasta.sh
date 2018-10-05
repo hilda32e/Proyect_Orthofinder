af29fbf2a3573bd391886ad8182dc4a53f9b4ae8
Hilda E. Ramos_Aboites <43645493+hilda32e@users.noreply.github.com>
2018-10-05 09:46:18 -0500

    Update script_download_fasta.sh

--- script_download_fasta.sh
+++ script_download_fasta.sh
@@ -6,7 +6,7 @@
 mkdir fasta_files_nema
 
 
-#Ciclo que extrae los datos del archivo $1 la columna 2 (-f2) y ejecuta el wget -P que Permite la descarga de los links de nuestra lista  $1 y con -P permitió que en la carpeta creada se almacenen os archivos FASTA descargados de los links.
+#Ciclo que extrae los datos del archivo $1 la columna 2 (-f2) y ejecuta el wget -P que Permite la descarga de los links de nuestra lista  $1 y con -P permitió que en la carpeta creada se almacenen los archivos FASTA descargados de los links.
 for x in $(cut -f2 $1)
 do
 wget -P fasta_files_nema $x
